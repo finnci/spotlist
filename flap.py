b784bc9a57cf9b993a89c5600b58fdfd097f6a8d
finnci <cfinn@metricfire.net>
2016-11-12 09:45:52 +0000

    add reqs & conf file

--- flap.py
+++ flap.py
@@ -6,7 +6,6 @@ import json
 SPOTIFY_APP_ID = '0e78fc3c02324da885aab026fe730863'
 SPOTIFY_APP_SECRET = '5d86d3b7fc1b4e6dae07c64e0ff0af3a'
 
-
 app = Flask(__name__)
 app.debug = True
 app.secret_key = 'development'
@@ -19,12 +18,13 @@ spotify = oauth.remote_app(
     # Change the scope to match whatever it us you need
     # list of scopes can be found in the url below
     # https://developer.spotify.com/web-api/using-scopes/
-    request_token_params={'scope': ('user-read-private','playlist-modify-public')},
+    request_token_params={
+        'scope': ('user-read-private', 'playlist-modify-public')
+    },
     base_url='https://accounts.spotify.com',
     request_token_url=None,
     access_token_url='/api/token',
-    authorize_url='https://accounts.spotify.com/authorize'
-)
+    authorize_url='https://accounts.spotify.com/authorize')
 
 
 @app.route('/')
@@ -37,8 +37,7 @@ def login():
     callback = url_for(
         'spotify_authorized',
         next=request.args.get('next') or request.referrer or None,
-        _external=True
-    )
+        _external=True)
     return spotify.authorize(callback=callback)
 
 
@@ -47,22 +46,25 @@ def spotify_authorized():
     resp = spotify.authorized_response()
     if resp is None:
         return 'Access denied: reason={0} error={1}'.format(
-            request.args['error_reason'],
-            request.args['error_description']
-        )
+            request.args['error_reason'], request.args['error_description'])
     if isinstance(resp, OAuthException):
         return 'Access denied: {0}'.format(resp.message)
 
     session['oauth_token'] = (resp['access_token'], '')
-    data = requests.get('https://api.spotify.com/v1/me', headers={ 'Authorization': 'Bearer ' + session.get('oauth_token')[0]}).json()
+    data = requests.get(
+        'https://api.spotify.com/v1/me',
+        headers={'Authorization': 'Bearer ' + session.get('oauth_token')[0]
+                 }).json()
     session['spot_id'] = data['id']
     print 'Logged in %s' % data
- 
+
     return redirect('/playlister')
+
 
 @app.route('/playlister')
 def playlister():
     return render_template('playlister.html')
+
 
 @app.route('/create/playlist/<name>')
 def ui_create_playlist(name):
@@ -71,20 +73,27 @@ def ui_create_playlist(name):
 
 
 def populate_playlist(playlist_id, tracklist, artist):
-    playlist_post = 'https://api.spotify.com/v1/users/%s/playlists/%s/tracks' % (session.get('spot_id'), '2tAcbeaDEk0357Q4YZ7QEU')
-    headers = { 'Authorization': 'Bearer ' + session.get('oauth_token')[0],
-               'content_type': 'application/json'}
+    playlist_post = 'https://api.spotify.com/v1/users/%s/playlists/%s/tracks' % (
+        session.get('spot_id'), '2tAcbeaDEk0357Q4YZ7QEU')
+    headers = {
+        'Authorization': 'Bearer ' + session.get('oauth_token')[0],
+        'content_type': 'application/json'
+    }
     # call get track uris
     track_uris = track_searcher(tracklist, artist)
-    return requests.post(playlist_post, headers=headers, data=json.dumps(track_uris))
+    return requests.post(
+        playlist_post, headers=headers, data=json.dumps(track_uris))
+
 
 def track_searcher(tracklist, artist):
-    track_search= 'https://api.spotify.com/v1/search?q='
+    track_search = 'https://api.spotify.com/v1/search?q='
     track_uris = []
-    headers = { 'Authorization': 'Bearer ' + session.get('oauth_token')[0],
-                'content_type': 'application_json'}
+    headers = {
+        'Authorization': 'Bearer ' + session.get('oauth_token')[0],
+        'content_type': 'application_json'
+    }
     import urlparse
-    from urllib import urlencode 
+    from urllib import urlencode
     params = {'track': 'finnplaceholder', 'artist': artist}
     url_parts = list(urlparse.urlparse(url))
     query = dict(urlparse.parse_qsl(url_parts[4]))
@@ -94,18 +103,19 @@ def track_searcher(tracklist, artist):
     for track in tracklist:
         finalurl.replace('finnplaceholder', track)
         track_get = requests.get(final_url, headers=headers)
-   
 
-       
-       
-def create_playlist( playlist_data):
+
+def create_playlist(playlist_data):
     create_playlist_url = 'https://api.spotify.com/v1/users/1155665355/playlists'
-    headers = { 'Authorization': 'Bearer ' + session.get('oauth_token')[0],
-               'content_type': 'application_json'}
-    post_plist = requests.post(create_playlist_url, headers=headers,  data=json.dumps(playlist_data))
-        # success, return the response object
+    headers = {
+        'Authorization': 'Bearer ' + session.get('oauth_token')[0],
+        'content_type': 'application_json'
+    }
+    post_plist = requests.post(
+        create_playlist_url, headers=headers, data=json.dumps(playlist_data))
+    # success, return the response object
     return post_plist
-                
+
 
 @spotify.tokengetter
 def get_spotify_oauth_token():

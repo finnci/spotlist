9ae0b03a68c7c25d48a2addbedeb06261908e8a5
finnci <cfinn@metricfire.net>
2016-12-28 15:16:51 +0000

    setlistfm search by artist, create frontend list, add in colour scheme

--- flap.py
+++ flap.py
@@ -1,12 +1,13 @@
 from flask import Flask, redirect, url_for, session, request, render_template
 from flask_oauthlib.client import OAuth, OAuthException
+from flask import Response
 import requests
 import json
 
 SPOTIFY_APP_ID = '0e78fc3c02324da885aab026fe730863'
 SPOTIFY_APP_SECRET = '5d86d3b7fc1b4e6dae07c64e0ff0af3a'
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='stat', static_url_path="")
 app.debug = True
 app.secret_key = 'development'
 oauth = OAuth(app)
@@ -51,10 +52,13 @@ def spotify_authorized():
         return 'Access denied: {0}'.format(resp.message)
 
     session['oauth_token'] = (resp['access_token'], '')
+
     data = requests.get(
         'https://api.spotify.com/v1/me',
-        headers={'Authorization': 'Bearer ' + session.get('oauth_token')[0]
-                 }).json()
+        headers={'Authorization':
+                 'Bearer ' + session.get('oauth_token')[0]}).json()
+
+    #headers={'Authorization': 'Bearer ' + session.get('oauth_token')[0]}).json()
     session['spot_id'] = data['id']
     print 'Logged in %s' % data
 
@@ -92,6 +96,7 @@ def track_searcher(tracklist, artist):
         'Authorization': 'Bearer ' + session.get('oauth_token')[0],
         'content_type': 'application_json'
     }
+    # todo: find a nice way to format this right.
     import urlparse
     from urllib import urlencode
     params = {'track': 'finnplaceholder', 'artist': artist}
@@ -103,6 +108,26 @@ def track_searcher(tracklist, artist):
     for track in tracklist:
         finalurl.replace('finnplaceholder', track)
         track_get = requests.get(final_url, headers=headers)
+
+@app.route('/set/search/<artist>')
+def search_setlist(artist):
+    # takes str artist name,
+    # returns list of str track names
+    set_search = 'https://api.setlist.fm/rest/0.1/search/setlists.json?artistName=%s' % (artist)
+    sets = requests.get(set_search)
+    res_dict = {'sets': set([])}
+    # sets is a list of setlists, 0 = most_recent (probably not worth using).
+    for mset in sets.json()['setlists']['setlist']:
+        # s looks like {'sets':{'set':[]}}
+        try:
+            for song_set in mset['sets']['set']:
+                for slist in song_set['song']:
+                    res_dict['sets'].add(slist['@name'])
+        except (AttributeError, TypeError, KeyError):
+            pass
+    res_d2 = {'sets': list(res_dict['sets'])}
+    resp = Response(json.dumps(res_d2), status=200, mimetype='application/json')
+    return resp
 
 
 def create_playlist(playlist_data):

64a540800fff727e9859c28dd69cae2e6922eaa6
finnci <cfinn@metricfire.net>
2017-01-01 20:36:20 +0000

    update to parse all setlists returned from setlist.fm, look for ratio match of artist names between what a user types and what gets found in spotify

--- flap.py
+++ flap.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, session, request, render_template
 from flask_oauthlib.client import OAuth, OAuthException
 from flask import Response
 from urllib import urlencode
+from difflib import SequenceMatcher
 import urlparse
 import requests
 import logging
@@ -12,9 +13,11 @@ import socket
 SPOTIFY_APP_ID = '<INSERT_ID>'
 SPOTIFY_APP_SECRET = '<INSERT_SECRETER_KEY'
 HG_KEY = '<HG_KEY>'
+APP_KEY = '<APP_KEY>'
 
 app = Flask(__name__, static_folder='stat', static_url_path="")
 app.debug = False
+app.secret_key = APP_KEY
 oauth = OAuth(app)
 
 spotify = oauth.remote_app(
@@ -49,7 +52,10 @@ def login():
 
 @app.route('/login/authorized')
 def spotify_authorized():
-    resp = spotify.authorized_response()
+    try:
+        resp = spotify.authorized_response()
+    except OAuthException:
+        return redirect('/login')
     if resp is None:
         return 'Access denied: reason={0} error={1}'.format(
             request.args['error_reason'], request.args['error_description'])
@@ -57,12 +63,11 @@ def spotify_authorized():
         return 'Access denied: {0}'.format(resp.message)
 
     session['oauth_token'] = (resp['access_token'], '')
-
-    data = requests.get(
-        'https://api.spotify.com/v1/me',
-        headers={'Authorization':
-                 'Bearer ' + session.get('oauth_token')[0]}).json()
-
+    try:
+        data = requests.get('https://api.spotify.com/v1/me',
+                             headers={'Authorization':'Bearer ' + session.get('oauth_token')[0]}).json()
+    except OAuthException:
+        redirect('/login')
     session['spot_id'] = data['id']
 
     return redirect('/playlister')
@@ -83,14 +88,14 @@ def ui_create_playlist():
     songs = r_data['songs']
     track_uris = track_searcher(songs, artist)
     p_list = create_playlist({'name': 'Playlist for gig: %s' % artist}).json()
-    emblink = "https://embed.spotify.com/?uri=%s" % p_list['uri']
+    emblink = "https://embed.spotify.com/?uri=%s" % p_list.get('uri')
     pid = p_list['id']
     conn = socket.create_connection(("7f590b16.carbon.hostedgraphite.com", 2003))
     if track_uris:
         plist_pop = populate_playlist(pid, track_uris)
     else:
         conn.send("%s.playlist.failed 1\n" % HG_KEY)
-        return Response(json.dumps({'error': 'No tracks found.'}),
+        return Response(json.dumps({'error': ['No tracks found, check you spelt everything right.']}),
                         status=500,
                         mimetype='application/json')
     conn.send("%s.playlist.created 1\n" % HG_KEY)
@@ -130,12 +135,16 @@ def track_searcher(tracklist, artist):
         try:
             for trk in track_get.json()['tracks']['items']:
                 try:
-                    if artist.lower() == str(trk['artists'][0]['name']).lower():
+                    artist = artist.lower()
+                    artist = artist.replace(' ' ,'')
+                    given = str(trk['artists'][0]['name']).lower()
+                    given = given.replace(' ', '')
+                    if similar(artist, given) > 0.6:
                         track_uris.add(trk['uri'])
                         break
                 except UnicodeError:
                     pass
-        except IndexError:
+        except (IndexError, KeyError):
             pass
     return track_uris
 
@@ -158,27 +167,40 @@ def search_setlist(artist):
     # returns list of str track names
     set_search = 'https://api.setlist.fm/rest/0.1/search/setlists.json?artistName=%s' % (artist)
     sets = requests.get(set_search)
+    conn = socket.create_connection(("7f590b16.carbon.hostedgraphite.com", 2003))
+    conn.send("%s.search.attempt 1\n" % (HG_KEY))
     res_dict = {'sets': set([])}
     # sets is a list of setlists, 0 = most_recent (probably not worth using).
-    for mset in sets.json()['setlists']['setlist']:
-        # s looks like {'sets':{'set':[]}}
-        try:
-            for song_set in mset['sets']['set']:
-                for slist in song_set['song']:
-                    res_dict['sets'].add(slist['@name'])
-        except (AttributeError, TypeError, KeyError) as e:
-            logging.debug('Err was: %s' % e)
-            pass
+    try:
+        for mset in sets.json()['setlists']['setlist']:
+            try:
+                for song_set in mset['sets']['set']:
+                    if song_set in ['song', u'song']:
+                        for slist in mset['sets']['set']['song']:
+                            res_dict['sets'].add(slist['@name'])
+                    else:
+                        for slist in song_set['song']:
+                            res_dict['sets'].add(slist['@name'])
+            except TypeError as e:
+                logging.error("Artist was %s, mset was: %s" % (artist, mset))
+    except ValueError:
+        pass
     res_d2 = {'sets': list(res_dict['sets'])}
     if not res_d2['sets']:
+        conn.send("%s.search.fail 1\n" % (HG_KEY))
         res_d2['sets'] = ["Can't find this one, sorry.",
                           "Guess you're a filthy hipster.",
                           "Or maybe you spelt the artists name wrong."
                           "Or its not available on setlist.fm for some reason"]
-
+    else:
+        conn.send("%s.search.success 1\n" % (HG_KEY))
     resp = Response(json.dumps(res_d2), status=200, mimetype='application/json')
+    conn.close()
     return resp
 
+
+def similar(a, b):
+    return SequenceMatcher(None, a, b).ratio()
 
 def create_playlist(playlist_data):
     create_playlist_url = 'https://api.spotify.com/v1/users/%s/playlists' % session.get('spot_id')
